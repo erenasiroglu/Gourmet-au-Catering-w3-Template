1dc23d2219a4f7ead0e135338a8ebd1011215f46
Eren Nasıroğlu <101004391+erenasiroglu@users.noreply.github.com>
2022-11-14 22:37:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var twoSum = function (nums, target) {
   for (var i = 0; i < nums.length; i++) {
-    for (var j = i + 1; j < nums.length; j++) {
+    for (var j = j + 1; j < nums.length; j++) {
       if (nums[i] + nums[j] === target) {
         return [i, j];
       }
